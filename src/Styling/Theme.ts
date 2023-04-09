c9a27e049b7d761ea4f39a63ada08ce628852e86
Casper Kristiansson <casper.kristiansson@yahoo.se>
2023-04-09 22:37:32 +0200

    init theme colors

--- src/Styling/Theme.ts
+++ src/Styling/Theme.ts
@@ -1,7 +1,31 @@
 export interface Theme {
+    buttonPrimary: string;
+
+    backgroundPrimary: string;
+    backgroundPrimaryLight: string;
+    backgroundSecondary: string;
+
+    navbar: string;
+
+    textDark: string;
+    textWhite: string;
+    textGreen: string;
+    textGray: string;
 }
 
-export const initializeTheme = (darkMode: boolean = false) => {
+export const initializeTheme = () => {
     return {
+        buttonPrimary: "#75BC5B",
+
+        backgroundPrimary: "#75BC5B",
+        backgroundPrimaryLight: "AFE29D",
+        backgroundSecondary: "#D9D9D9",
+
+        navbar: "#141416",
+
+        textDark: "#555555",
+        textWhite: "#FFFFFF",
+        textGreen: "#75BC5B",
+        textGray: "#939393"
     } as Theme;
 }
