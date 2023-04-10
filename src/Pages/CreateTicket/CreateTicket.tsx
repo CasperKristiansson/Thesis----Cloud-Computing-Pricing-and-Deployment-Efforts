d7f349bc782cbf375eb231e33ae7fb80c90c94cd
Casper Kristiansson <casper.kristiansson@yahoo.se>
2023-04-10 13:45:24 +0200

    Fix centering and label

--- src/Pages/CreateTicket/CreateTicket.tsx
+++ src/Pages/CreateTicket/CreateTicket.tsx
@@ -4,12 +4,16 @@ import Stepper from '@mui/material/Stepper';
 import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
 import { StepperComponent } from '../../Components/StepperComponent';
+import { Typography } from '@mui/material';
 
 export const CreateTicket: React.FC<{}> = () => {
   return (
-		<Box sx={{ display: 'flex', justifyContent: 'center' }}>
+		<Box sx={{ display: 'flex', justifyContent: 'center', paddingTop: "10px" }}>
 			<Box sx={{ width: '700px' }}>
-				<Stepper activeStep={0} alternativeLabel>
+				<Typography variant="h2" textAlign={"center"}>
+					Create Ticket
+				</Typography>
+				<Stepper activeStep={0} alternativeLabel sx={{ marginTop: "35px"}}>
 					{[1,2,3].map((label) => (
 						<Step key={label}>
 								<StepLabel StepIconComponent={StepperComponent} />
