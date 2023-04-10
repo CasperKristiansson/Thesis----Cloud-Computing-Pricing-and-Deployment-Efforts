5bc4226abd8a94c8d5320605fb78398108c7bdb0
Casper Kristiansson <casper.kristiansson@yahoo.se>
2023-04-10 09:58:47 +0200

    Convert to jss

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,28 +1,140 @@
 import { Box, Button, Grid, Typography } from '@mui/material';
+import { createUseStyles } from 'react-jss';
+import { Link } from 'react-router-dom';
 import Logo from '../../Assets/Logo.png';
 
+const useStyles = createUseStyles({
+  root: {
+    height: 'calc(100%)',
+    overflow: 'hidden',
+  },
+  videoWrapper: {
+    height: '60%',
+    position: 'relative',
+    zIndex: 0,
+    color: 'white',
+    overflow: 'hidden',
+  },
+  video: {
+    position: 'absolute',
+    width: '100%',
+    top: 0,
+    left: 0,
+    overflowY: 'hidden',
+    zIndex: 0,
+  },
+  logoWrapper: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    textAlign: 'center',
+  },
+  logo: {
+    height: 50,
+  },
+  title: {
+    fontSize: '3rem',
+    fontFamily: "'Poppins', sans-serif",
+    fontWeight: 'bold',
+    marginTop: 2,
+  },
+  buttonsWrapper: {
+    zIndex: 2,
+		marginTop: 40,
+  },
+  registerButton: {
+    backgroundColor: 'primary.main',
+    '&:hover': {
+      backgroundColor: 'primary.dark',
+    },
+  },
+  loginButton: {
+    borderColor: 'primary.main',
+    color: 'primary.main',
+    '&:hover': {
+      borderColor: 'primary.dark',
+      color: 'primary.dark',
+    },
+  },
+  aboutWrapper: {
+    height: '40%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    zIndex: 1,
+    backgroundColor: '#fffff',
+  },
+  aboutBox: {
+    width: '450px',
+    marginLeft: "25%",
+    marginRight: 'auto',
+    textAlign: 'left',
+  },
+  aboutTitle: {
+    marginBottom: 2,
+    fontFamily: "'Poppins', sans-serif",
+  },
+  aboutText: {
+    textAlign: 'left',
+  },
+});
+
 export const Home: React.FC<{}> = (): JSX.Element => {
+	const classes = useStyles();
+
   return (
 		<>
-		<Grid container direction="column" sx={{ height: "calc(100%)", overflow: "hidden" }}>
-      <Grid item sx={{ height: '60%', position: 'relative', zIndex: 0, color: "white", overflow: "hidden" }}>
-			<video src="https://video.wixstatic.com/video/11062b_456cb078f5fb4deaa2845361aa665cf0/1080p/mp4/file.mp4" autoPlay muted loop style={{ position: 'absolute', width: '100%', top: 0, left: 0, overflowY: "hidden" , zIndex: 0}} />
-        <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', textAlign: 'center' }}>
-          <img src={Logo} alt="Ticket System" style={{ height: 50 }} />
-          <Box sx={{ fontSize: '3rem', fontFamily: "'Poppins', sans-serif", fontWeight: 'bold', mt: 2 }}>Ticket System</Box>
-          <Box sx={{ mt: 2, zIndex: 2 }}>
-            <Button variant="contained" color="primary">Register</Button>
-            <Button variant="outlined" color="primary" sx={{ ml: 2 }}>Login</Button>
+      <Grid container direction="column" className={classes.root}>
+        <Grid item className={classes.videoWrapper}>
+          <video
+            src="https://video.wixstatic.com/video/11062b_456cb078f5fb4deaa2845361aa665cf0/1080p/mp4/file.mp4"
+            autoPlay
+            muted
+            loop
+            className={classes.video}
+          />
+          <Box className={classes.logoWrapper}>
+            <img src={Logo} alt="Ticket System" className={classes.logo} />
+            <Box className={classes.title}>Ticket System</Box>
+            <Box className={classes.buttonsWrapper}>
+              <Button
+                variant="contained"
+                className={classes.registerButton}
+                component={Link}
+                to="/register"
+              >
+                Register
+              </Button>
+              <Button
+                variant="outlined"
+                className={classes.loginButton}
+                sx={{ ml: 2 }}
+                component={Link}
+                to="/login"
+              >
+                Login
+              </Button>
+            </Box>
           </Box>
-        </Box>
-      </Grid>
-      <Grid item sx={{ height: '40%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', zIndex: 1, backgroundColor: "#fffff" }}>
-				<Box sx={{ width: '400px', ml: 40, mr: 'auto', textAlign: 'left' }}>
-					<Typography variant="h3" sx={{ mb: 2, fontFamily: "'Poppins', sans-serif", }}>About the Ticket System</Typography>
-					<Typography variant="body1" sx={{ textAlign: 'left' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae risus auctor, auctor ipsum nec, tempus neque. Donec sagittis quam at eros hendrerit euismod. Fusce finibus ultrices est, id varius nibh. </Typography>
-				</Box>
-      </Grid>
-    </Grid>
+        </Grid>
+        <Grid item className={classes.aboutWrapper}>
+          <Box className={classes.aboutBox}>
+						<Typography variant="h4" className={classes.aboutTitle}>
+							About the Ticker System
+						</Typography>
+						<Typography variant="body1" className={classes.aboutText}>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+							quis nisl euismod, aliquam nisl eu, aliquet nisl. Donec
+							condimentum, nisl eget aliquam lacinia, nisl nisl aliquet
+							ligula, eget aliquam nisl nisl eget nisl. Donec condimentum,
+							nisl eget aliquam lacinia, nisl nisl aliquet ligula, eget
+							aliquam nisl nisl eget nisl.
+						</Typography>
+					</Box>
+				</Grid>
+			</Grid>
 		</>
 	);
 };
