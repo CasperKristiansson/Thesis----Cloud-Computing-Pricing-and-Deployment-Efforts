8e4a22a1dd7bdb7a37905024a0fa8b0c1ca190ff
Fredrik <fredrik.thomas.lundstrom@gmail.com>
2023-04-16 13:08:41 +0200

    updated secret names

--- TestDBConnection/__init__.py
+++ TestDBConnection/__init__.py
@@ -9,16 +9,16 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     load_dotenv()
 
-    server = os.getenv("SERVER")
-    user = os.getenv("USER")
-    password = os.getenv("PASSWORD")
-    database = os.getenv("DATABASE")
+    server = os.getenv("DB_SERVER")
+    username = os.getenv("DB_USERNAME")
+    password = os.getenv("DB_PASSWORD")
+    database = os.getenv("DB_DATABASE")
 
-    if(server == None or user == None or password == None or database == None):
+    if(server == None or username == None or password == None or database == None):
         return func.HttpResponse(f"Environment Variables not set or could not be loaded.")
 
     try:
-        conn = pymssql.connect(server, user, password, database)
+        conn = pymssql.connect(server, username, password, database)
         cursor = conn.cursor(as_dict=True)
         return func.HttpResponse(f"Connection Successful")
     except Exception as e:
