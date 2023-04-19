43e307f9a09add1a6cf43b4632f4a22a7f46d1a9
Casper Kristiansson <casper.kristiansson@yahoo.se>
2023-04-19 17:15:02 +0200

    Saga action for login

--- src/Redux/Saga.ts
+++ src/Redux/Saga.ts
@@ -1,7 +1,26 @@
-import { all } from "redux-saga/effects";
+import { all, put, takeLatest } from "redux-saga/effects";
+import { LOGIN, SET_IN_LINE_OPERATION_IN_PROGRESS, SET_TOKEN } from "./Actions";
+import { loginRequest } from "../authConfig";
+import { msalInstance } from "..";
+import { acquireTokenSilent } from "./helpers";
+import { AuthenticationResult } from "@azure/msal-browser";
+
+function* handleLogin(action: any) {
+  yield put({ type: SET_IN_LINE_OPERATION_IN_PROGRESS, payload: true })
+  try {
+    msalInstance.loginPopup(loginRequest);
+    const graphTokenResponse: AuthenticationResult = yield acquireTokenSilent(loginRequest, msalInstance);
+    yield put({ type: SET_TOKEN, payload: graphTokenResponse.accessToken });
+  } catch (error) {
+    console.log(error);
+  } finally {
+    yield put({ type: SET_IN_LINE_OPERATION_IN_PROGRESS, payload: false })
+  }
+}
 
 function* projectSaga() {
   yield all([
+    takeLatest(LOGIN, handleLogin),
   ]);
 }
   
--- src/Redux/helpers.ts
+++ src/Redux/helpers.ts
@@ -1,1 +1,7 @@
-export function temp() {}
+import { AuthenticationResult } from "@azure/msal-browser";
+
+export async function acquireTokenSilent(loginRequest: any, msalInstance: any): Promise<AuthenticationResult> {
+    return await msalInstance.acquireTokenSilent({
+        ...loginRequest
+    });
+}
