9c6012605daa397dbc702c68c511d4a8ba8625ec
Casper Kristiansson <casper.kristiansson@yahoo.se>
2023-04-27 11:02:59 -0400

    Edit Project

--- src/ComponentRouting.tsx
+++ src/ComponentRouting.tsx
@@ -8,6 +8,7 @@ import { CreateProject } from './Pages/CreateProject/CreateProject';
 import { Login } from './Pages/Login/Login';
 import { IndividualTicket } from './Pages/IndividualTicket/IndividualTicket';
 import { EditTicket } from './Pages/EditTicket/EditTicket';
+import { EditProject } from './Pages/EditProject/EditProject';
 
 export const ComponentRouting: React.FC<{dispatch: any}> = ({ dispatch }) => {
 	return (
@@ -21,6 +22,7 @@ export const ComponentRouting: React.FC<{dispatch: any}> = ({ dispatch }) => {
 				<Route path="/login" element={<Login dispatch={dispatch} />} />
 				<Route path="/ticket/:id" element={<IndividualTicket dispatch={dispatch} />} />
 				<Route path="/edit-ticket/:id" element={<EditTicket dispatch={dispatch} />} />
+				<Route path="edit-project/:id" element={<EditProject dispatch={dispatch} />} />
 			</Routes>
 		</div>
 	);
---
+++ src/Pages/EditProject/ChildComponents/StepTwo.tsx
@@ -0,0 +1,67 @@
+import { Theme } from '../../../Styling/Theme';
+import { createUseStyles } from 'react-jss';
+import { Typography, Grid, Paper } from '@mui/material';
+import { useSelector } from 'react-redux';
+import { AppDispatch } from '../../../store';
+import { getCreateProjectAssociatedCompany, getCreateProjectDescription, getCreateProjectName } from '../../../Redux/Selectors';
+
+const useStyles = createUseStyles((theme: Theme) => {
+  return {
+    root: {
+			height: 'calc(100% - 80px)',
+    },
+		paper: {
+      height: '100%',
+			padding: 5,
+			backgroundColor: theme.backgroundSecondaryLight,
+    },
+		paperInner: {
+			display: 'flex',
+      flexDirection: 'column',
+		},
+    row: {
+      textAlign: 'left',
+    },
+  };
+});
+
+export const StepTwo: React.FC<{dispatch: AppDispatch}> = ({ dispatch }) => {
+  const projectName = useSelector(getCreateProjectName);
+  const associatedCompany = useSelector(getCreateProjectAssociatedCompany);
+  const companyDescription = useSelector(getCreateProjectDescription);
+  const classes = useStyles();
+
+  return (
+		<>
+		<div className={classes.root}>
+			<Typography variant="h5" textAlign={"center"} sx={{ paddingBottom: 2 }}>
+				Project Information
+			</Typography>
+      <Paper className={classes.paper}>
+				<div className={classes.paperInner}>
+					<Grid container spacing={3} height="100%">
+						<Grid item xs={12} sm={6} className={classes.row}>
+							<Typography variant="h6">
+								Project Name
+							</Typography>
+							<Typography variant="body1">{projectName}</Typography>
+						</Grid>
+						<Grid item xs={12} sm={6} className={classes.row}>
+							<Typography variant="h6">
+								Associated Company
+							</Typography>
+							<Typography variant="body1">{associatedCompany}</Typography>
+						</Grid>
+						<Grid item xs={12} className={classes.row}>
+							<Typography variant="h6">
+								Company Description
+							</Typography>
+							<Typography variant="body1">{companyDescription}</Typography>
+						</Grid>
+					</Grid>
+				</div>
+      </Paper>
+    </div>
+    </>
+  );
+}
