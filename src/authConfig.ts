1386e6b979967386335804dc0809ded59300f0b5
Fredrik <fredrik.thomas.lundstrom@gmail.com>
2023-04-29 14:57:47 +0200

    Updated configuration for microsoft authentication

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -4,9 +4,8 @@ import { Configuration, PopupRequest } from "@azure/msal-browser";
 export const msalConfig: Configuration = {
     auth: {
         clientId: "0a74a678-aba6-4b17-957d-98fd60b7fe5e",
-        authority: "https://login.microsoftonline.com/common",
-        redirectUri: "/",
-        postLogoutRedirectUri: "/"
+        authority: "https://login.microsoftonline.com/5cb1f273-8ffe-4f6e-ad5a-02dbc3e19e3e",
+        redirectUri: "http://localhost:3000/"
     },
     system: {
         allowNativeBroker: false // Disables WAM Broker
@@ -15,7 +14,9 @@ export const msalConfig: Configuration = {
 
 // Add here scopes for id token to be used at MS Identity Platform endpoints.
 export const loginRequest: PopupRequest = {
-    scopes: ["User.Read"]
+    scopes: ["User.Read", "api://0a74a678-aba6-4b17-957d-98fd60b7fe5e/user_impersonation",],
+    authority: "https://login.microsoftonline.com/5cb1f273-8ffe-4f6e-ad5a-02dbc3e19e3e",
+    redirectUri: "http://localhost:3000/"
 };
 
 // Add here the endpoints for MS Graph API services you would like to use.
