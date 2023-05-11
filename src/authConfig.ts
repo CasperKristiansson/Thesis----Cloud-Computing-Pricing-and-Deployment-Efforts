03d0b142d2e6d49e16a023f50285312f72dabf5f
Fredrik Lundström <70958591+frlundst@users.noreply.github.com>
2023-05-11 12:18:29 +0200

    Update authConfig.ts

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -5,7 +5,7 @@ export const msalConfig: Configuration = {
     auth: {
         clientId: "0a74a678-aba6-4b17-957d-98fd60b7fe5e",
         authority: "https://login.microsoftonline.com/5cb1f273-8ffe-4f6e-ad5a-02dbc3e19e3e",
-        redirectUri: window.location.hostname === "localhost ? ""http://localhost:3000/" : "https://zealous-stone-03a19ae03.3.azurestaticapps.net/",
+        redirectUri: window.location.hostname === "localhost" ? "http://localhost:3000/" : "https://zealous-stone-03a19ae03.3.azurestaticapps.net/",
     },
     system: {
         allowNativeBroker: false, // Disables WAM Broker
@@ -17,7 +17,7 @@ export const msalConfig: Configuration = {
 export const loginRequest: PopupRequest = {
     scopes: ["User.Read", "api://0a74a678-aba6-4b17-957d-98fd60b7fe5e/user_impersonation",],
     authority: "https://login.microsoftonline.com/5cb1f273-8ffe-4f6e-ad5a-02dbc3e19e3e",
-    redirectUri: window.location.hostname === "localhost ? ""http://localhost:3000/" : "https://zealous-stone-03a19ae03.3.azurestaticapps.net/"
+    redirectUri: window.location.hostname === "localhost" ? "http://localhost:3000/" : "https://zealous-stone-03a19ae03.3.azurestaticapps.net/"
 };
 
 // Add here the endpoints for MS Graph API services you would like to use.
